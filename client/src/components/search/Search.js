0204f0a48a794c46dac2b2ad6591e49f1eb49a0c
Pignataro67 <Pignataro67@gmail.com>
2019-04-17 12:11:50 -0700

    adding state and setting state in Search component

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import SearchInput from './SearchInput';
+import Button from '../Button';
+import Card from '../Card';
 
-class Search extends Component{
+class Search extends Component {
+
+  state = {
+    startingLocation: '',
+    destination: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  handleChangeStart = ({value: startingLocation}) => {
+    this.setState({
+      startingLocation
+    })
+  }
+  
   render() {
     return (
       <form>
