0040e3be1188dc603d2a865d3c02c4edac52a03e
Pignataro67 <Pignataro67@gmail.com>
2019-04-20 09:45:52 -0700

    trying to integrate fetch functions in actions

--- client/src/actions/fetchLocations.js
+++ client/src/actions/fetchLocations.js
@@ -1,3 +1,26 @@
+function _fetchLocation(input) {
+  return fetch(`/search_start/${input}`)
+  .then(res => res.json())
+  .then(locations => locations.predictions)
+}
+
 export function fetchStartingLocation(input) {
   console.log(input)
+    return (dispatch) => {
+      dispatch({ type: 'FETCHING_SUGGESTED_START_LOCATIONS' });
+
+      _fetchLocation(input).then(suggestedStartingLocations =>
+    dispatch({ type: 'DISPLAY_START_LOCATIONS',
+    suggestedStartingLocations }));
+  };
+}
+
+export function fetchDropOff(input) {
+  console.log(input)
+    return (dispatch) => {
+      dispatch({ type: 'FETCHING_SUGGESTED_DROPOFF' });
+      _fetchLocation(input).then(suggestedDropOffs => 
+    dispatch({ type: 'DISPLAY_DROPOFFS', suggestedDropoffs
+    }));
+  };
 }
